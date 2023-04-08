b3a213e0c837ac841bafc064ab5ae676ffb23d26
kitokun <pipin3seipippin@gmail.com>
2023-04-09 00:16:00 +0900

    URL修正

--- src/scripts/api/fetchTransactions.js
+++ src/scripts/api/fetchTransactions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function fetchTransactions() {
-    const res = await axios.get('http://0.0.0.0:8080/money_transaction');
+    const res = await axios.get('http://0.0.0.0:8080/transaction');
     console.log(res.data);
     return res.data;
 }
